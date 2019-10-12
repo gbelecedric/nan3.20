2c24ce9f4bd2a1296d837cc5d85ecabe67853042
gbelecedric <47278122+gbelecedric@users.noreply.github.com>
2019-10-12 11:22:21 +0000

    creation des routes

--- nan/nan_register/views.py
+++ nan/nan_register/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def register(request):
+    templates_name = 'pages/register.html'
+    data = {}
+    return render(request,templates_name,data)
